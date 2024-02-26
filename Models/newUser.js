da63747eba090f54db76399577268315845bfdf7
RahulkrWD <rahulkmrgaya21@gmail.com>
2024-02-26 19:08:07 +0530

    update model

--- Models/newUser.js
+++ Models/newUser.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
-const userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String,
-  phone: Number,
-  uniqueId: Number,
-});
+const userSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: String,
+    password: String,
+    phone: Number,
+    uniqueId: Number,
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("users", userSchema);
--- Models/order.js
+++ Models/order.js
@@ -1,15 +1,18 @@
 const mongoose = require("mongoose");
 
-const orderSchema = new mongoose.Schema({
-  orderId: Number,
-  name: String,
-  email: String,
-  address: String,
-  phone: Number,
-  cost: Number,
-  restName: String,
-  orderItems: String,
-  uniqueId: Number,
-});
+const orderSchema = new mongoose.Schema(
+  {
+    orderId: Number,
+    name: String,
+    email: String,
+    address: String,
+    phone: Number,
+    cost: Number,
+    restName: String,
+    orderItems: String,
+    uniqueId: Number,
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("orders", orderSchema);
